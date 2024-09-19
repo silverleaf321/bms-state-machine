59bbc2c4d6089a84cb6576496b6126658dae8d3b
Allison Yen <silverleaf@Allisons-MacBook-Pro.local>
2024-09-19 11:48:07 -0700

    new ish code

--- state_machine.py
+++ state_machine.py
@@ -1,36 +1,102 @@
-from states import ChargingState, DischargingState, IdleState, FaultState
-from transitions import Machine
-
-class BatteryManagementSystem:
+class BMSStateMachine:
     def __init__(self):
-        self.state_machine = Machine(model=self, states=[
-            ChargingState(),
-            DischargingState(),
-            IdleState(),
-            FaultState()
-        ], transitions=[
-            {'trigger': 'charge', 'source': 'Idle', 'dest': 'Charging'},
-            {'trigger': 'discharge', 'source': 'Idle', 'dest': 'Discharging'},
-            {'trigger': 'fault', 'source': '*', 'dest': 'Fault'},
-            {'trigger': 'recover', 'source': 'Fault', 'dest': 'Idle'},
-            {'trigger': 'complete_charge', 'source': 'Charging', 'dest': 'Idle'},
-            {'trigger': 'complete_discharge', 'source': 'Discharging', 'dest': 'Idle'}
-        ], initial='Idle')
+        self.state = 'off'
+        self.voltage = 0
+        self.temperature = 25  # initial temperature
+        self.fault = False
+        self.overcharge = False
+        self.overdischarge = False
+    
+    def check_for_fault(self):
+        # add more code here, check for short circuit
+        return self.fault
+    
+    def check_for_extreme_temperatures(self):
+        if self.temperature < 0 or self.temperature > 60:  # temperature range, change this to correct values later
+            return True
+        return False
+    
+    def check_for_overcharge(self):
+        return self.overcharge
+    
+    def check_for_overdischarge(self):
+        return self.overdischarge
+    
+    def handle_key_in(self):
+        if self.state == 'off':
+            self.state = 'key_in'
+            print("key in")
+            self.check_fault_and_proceed()
+    
+    def check_fault_and_proceed(self):
+        if self.check_for_fault():
+            self.state = 'off'
+            print("fault detected, go to off state.")
+        else:
+            self.state = 'balance'
+            print("no fault detected, proceed")
+            self.pre_charge()
+    
+    def pre_charge(self):
+        # precharge to 90% of total voltage
+        self.voltage = 0.9 * 100  # change 100 to total voltage
+        print("precharge done")
+        self.state = 'idle'
+        self.idle()
+    
+    def idle(self):
+        print("In Idle state.")
+        self.state = 'discharge_data_received'
+        self.discharge_data_received()
+    
+    def discharge_data_received(self):
+        print("Discharge data received.")
+        if self.check_for_extreme_temperatures():
+            print("extreme temperatures detected, go to off state.")
+            self.state = 'off'
+        elif self.check_for_fault():
+            print("fault detected, go to off state.")
+            self.state = 'off'
+        else:
+            self.state = 'balance'
+            print("no faults, keep going")
+            self.balance()
+    
+    def balance(self):
+        print("Balancing the battery...")
+        if self.check_for_overdischarge():
+            print("overdischarge detected, go to discharge state.")
+            self.state = 'discharge'
+            self.discharge()
+        else:
+            print("battery balanced")
+            self.state = 'charge'
+            self.charge()
+    
+    def charge(self):
+        print("Charging the battery...")
+        if self.check_for_overcharge():
+            print("overcharge detected, go to balance.")
+            self.state = 'balance'
+        else:
+            print("charging complete, go to  off state.")
+            self.state = 'off'
+    
+    def discharge(self):
+        print("discharging battery")
+        if self.check_for_fault():
+            print("fault, go to off state")
+            self.state = 'off'
+        else:
+            print("discharge complete, to to off state")
+            self.state = 'off'
+    
+    def turn_key_off(self):
+        print("key turned off, go to off state.")
+        self.state = 'off'
 
-    def charge_battery(self):
-        self.charge()
-        
-    def discharge_battery(self):
-        self.discharge()
+bms = BMSStateMachine()
 
-    def trigger_fault(self):
-        self.fault()
-
-    def recover_from_fault(self):
-        self.recover()
-
-    def complete_charge(self):
-        self.complete_charge()
-
-    def complete_discharge(self):
-        self.complete_discharge()
+# key in/out
+bms.handle_key_in()
+bms.turn_key_off()
